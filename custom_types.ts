b69ce1a2e18a07cadd3ad403c57a7aa220ed8287
eloiji <eloisajocsoniting@gmail.com>
2025-09-18 16:05:34 +0800

    Added custom types

---
+++ custom_types.ts
@@ -0,0 +1,21 @@
+type Food = string;
+
+let FavoriteFood: Food = "Pizza";
+
+type Person = {
+    name: string;
+    age: number;
+    isStudent: boolean;
+}
+
+let person: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2: Person = {
+    name: "John",
+    age: 35,
+    isStudent: false
+}
