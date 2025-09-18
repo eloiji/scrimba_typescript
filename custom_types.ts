fd6fc6d186a3c0d1164506c8570473bb2320222b
eloiji <eloisajocsoniting@gmail.com>
2025-09-18 16:56:19 +0800

    Added type Address

--- custom_types.ts
+++ custom_types.ts
@@ -2,20 +2,37 @@ type Food = string;
 
 let FavoriteFood: Food = "Pizza";
 
+type Address = {
+    street: string;
+    city: string;
+    country: string;
+}
+
 type Person = {
     name: string;
     age: number;
     isStudent: boolean;
+    address: Address;
 }
 
 let person: Person = {
     name: "Joe",
     age: 42,
-    isStudent: true
+    isStudent: true,
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        country: "USA"
+    },
 }
 
 let person2: Person = {
     name: "John",
     age: 35,
-    isStudent: false
+    isStudent: false,
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        country: "USA"
+    },
 }
